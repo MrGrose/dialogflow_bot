1605b40009fd5c2c62b0151482ca787b3e799053
Roman <grachev.ro@gmail.com>
2025-05-18 13:34:43 +0300

    print moved to ifmain

--- create_intent.py
+++ create_intent.py
@@ -8,8 +8,6 @@ def create_intent(project_id, display_name, training_phrases_parts, message_text
     """Create an intent of the given intent type."""
     intents_client = dialogflow.IntentsClient()
 
-    intents_client = dialogflow.IntentsClient()
-
     parent = dialogflow.AgentsClient.agent_path(project_id)
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
@@ -24,11 +22,9 @@ def create_intent(project_id, display_name, training_phrases_parts, message_text
         display_name=display_name, training_phrases=training_phrases, messages=[message]
     )
 
-    response = intents_client.create_intent(
+    return intents_client.create_intent(
         request={"parent": parent, "intent": intent}
     )
-
-    print("Intent created: {}".format(response))
 
 
 def main() -> None:
@@ -41,9 +37,10 @@ def main() -> None:
     project_id = data.get("project_id")
 
     with open("text_questions.json", "r", encoding="UTF-8") as file:
-        questions = json.loads(file)
+        questions = json.loads(file.read())
         for title, contents in questions.items():
             create_intent(project_id, title, contents["questions"], [contents["answer"]])
+        print("Intent created!")
 
 
 if __name__ == "__main__":
